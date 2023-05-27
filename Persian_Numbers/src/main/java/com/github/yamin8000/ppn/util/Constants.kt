cfd0394d22a7830273441dfe933f1dc57846d0b6
YaMiN <yamin8000@yahoo.com>
2023-05-27 13:05:40 +0330

    closes #51

--- Persian_Numbers/src/main/java/com/github/yamin8000/ppn/util/Constants.kt
+++ Persian_Numbers/src/main/java/com/github/yamin8000/ppn/util/Constants.kt
@@ -5,16 +5,10 @@ import java.math.BigInteger
 
 internal object Constants {
 
-    private const val ZERO_ONLY_REGEX = "0+"
-    private const val DECIMAL_REGEX = "\\d*\\.\\d+"
-
     internal const val NaN = "NaN"
     internal const val zeroChar = '0'
     internal const val dashChar = '-'
     internal const val dotChar = '.'
-
-    internal val zeroOnlyRegex: Regex by lazy(LazyThreadSafetyMode.NONE) { Regex(ZERO_ONLY_REGEX) }
-    internal val decimalRegex: Regex by lazy(LazyThreadSafetyMode.NONE) { Regex(DECIMAL_REGEX) }
 
     internal val zeroBigInteger: BigInteger by lazy(LazyThreadSafetyMode.NONE) { BigInteger.ZERO }
     internal val oneBigInteger: BigInteger by lazy(LazyThreadSafetyMode.NONE) { BigInteger.ONE }
--- Persian_Numbers/src/test/java/com/github/yamin8000/ppn/MainTests.kt
+++ Persian_Numbers/src/test/java/com/github/yamin8000/ppn/MainTests.kt
@@ -6,24 +6,38 @@ import org.junit.jupiter.api.Test
 
 class MainTests {
 
-
     @Test
-    fun singleDigitTest() {
+    fun normalTests() {
         assertEquals("دوازده", PersianDigits.spellToPersian(12))
         assertEquals("صفر", PersianDigits.spellToPersian(0))
         assertEquals("دو", "02".spellToPersian())
+        assertEquals("یک", 1.spellToPersian())
+        assertEquals("نه", 9.spellToPersian())
     }
 
     @Test
     fun negativeTest() {
         assertEquals("منفی یکصد و بیست و چهار", PersianDigits.spellToPersian(-124))
         assertEquals("صفر", "-00".spellToPersian())
-        assertEquals("NaN", "-".spellToPersian())
         assertEquals("منفی یک دهم", "-.1".spellToPersian())
     }
 
     @Test
     fun decimalTest() {
         assertEquals("یکصد و بیست و چهار ممیز یک، صدم", PersianDigits.spellToPersian(124.01))
+        assertEquals("سه ممیز چهارده، صدم", 3.14.spellToPersian())
+    }
+
+    @Test
+    fun edgeCases() {
+        assertEquals(
+            "دوازده میلیارد و یکصد و بیست و سه میلیون و سیصد و دوازده هزار و یکصد و بیست و سه",
+            12_123_312_123.spellToPersian()
+        )
+        assertEquals(
+            "پنج میلیون و یکصد و بیست و یک هزار و سیصد و بیست و یک",
+            PersianDigits.spellToPersian("5121321")
+        )
+        assertEquals("NaN", "-".spellToPersian())
     }
 }
